abc4ff46f0c2bb99950234930c8ad818f655d0a2
aitherapp <aitherapp@users.noreply.github.com>
2026-01-27 22:29:44 +0100

    Fix caching issue: bump SW version and add cache cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aither-v1';
+const CACHE_NAME = 'aither-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -16,6 +16,18 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
@@ -25,3 +37,4 @@ self.addEventListener('fetch', event => {
             .then(response => response || fetch(event.request))
     );
 });
+
